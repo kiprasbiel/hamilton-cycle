f5e95d921a1da71249f498d36c4287ba36499610
Kipras <reg@anaweb.lt>
2020-04-30 19:56:38 +0300

    Adding graph-generator for randomizing huge matrixes

---
+++ graph-generator.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy
+import sys
+numpy.set_printoptions(threshold=sys.maxsize)
+
+G = nx.random_geometric_graph(200, 0.125)
+
+A = nx.adjacency_matrix(G)
+matrica = A.todense()
+file = open("matrica.txt", "w")
+file.write(str(matrica))
+file.close()
+# position is stored as node attribute data for random_geometric_graph
+pos = nx.get_node_attributes(G, 'pos')
+
+# find node near center (0.5,0.5)
+dmin = 1
+ncenter = 0
+for n in pos:
+    x, y = pos[n]
+    d = (x - 0.5)**2 + (y - 0.5)**2
+    if d < dmin:
+        ncenter = n
+        dmin = d
+print(ncenter)
+# color by path length from node near center
+p = dict(nx.single_source_shortest_path_length(G, ncenter))
+
+plt.figure(figsize=(8, 8))
+nx.draw_networkx_edges(G, pos, nodelist=[ncenter], alpha=0.4)
+nx.draw_networkx_nodes(G, pos, nodelist=list(p.keys()),
+                       node_size=80,
+                       node_color=list(p.values()),
+                       cmap=plt.cm.Reds_r)
+
+plt.xlim(-0.05, 1.05)
+plt.ylim(-0.05, 1.05)
+plt.axis('off')
+plt.show()
